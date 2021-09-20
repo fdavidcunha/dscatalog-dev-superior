276767eb7489fa203b9ed154eec8b8387271c6bc
David Ferreira da Cunha <david.ferreira@ao3tech.com>
2021-09-20 07:40:48 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,9 +3,19 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
@@ -51,7 +61,4 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
 }
--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -3,18 +3,24 @@ package com.devsuperior.dscatalog.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 /* Implementação do controlador REST, para a classe Category. */
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	
+	// Injetando uma dependência com a camada de serviço.
+	@Autowired
+	private CategoryService service;
 	
 	// Endpoint para listar todas as categorias e encapsular uma resposta HTTP. 
 	// @GetMapping informa que o método/end point será um serviço da API.
@@ -23,9 +29,7 @@ public class CategoryResource {
 	public ResponseEntity<List<Category>> findAll() {
 		
 		// List é uma interface, por isso se inicializa a variável com ArrayList, que é uma das implementações da List.
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Livros"));
-		list.add(new Category(2L, "Eletrônicos"));
+		List<Category> list = service.findAll();
 		
 		// Retornando a lista de categorias no corpo da resposta HTTP da requisição.
 		// ResponseEntity.ok informa que o código de retorno é 200 - Sucesso.
