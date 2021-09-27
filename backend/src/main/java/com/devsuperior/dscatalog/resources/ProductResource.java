72b6b284b90c55969310f2e29ac9000fac5769f5
David Ferreira da Cunha <david.ferreira@ao3tech.com>
2021-09-26 22:47:09 -0300

    Product repository, service, resource and get method

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -0,0 +1,110 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.net.URI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
+
+/* Implementação do controlador REST, para a classe Product.                                                                                             */
+/* Pontos importantes sobre esta classe:                                                                                                                  */
+/*                                                                                                                                                        */
+/* ResponseEntity<List<ProductDTO>> -> Retorna todos os registros do banco de dados, SEM paginação.                                                      */
+/* ResponseEntity<Page<ProductDTO>> -> Retorna todos os registros do banco de dados, COM paginação.                                                      */
+/* @GetMapping                       -> Informa que o método/end point será um serviço da API - responderá pelo verbo GET.                                */
+/* @PostMapping                      -> Informa que o método/end point será um serviço da API - responderá pelo verbo POST.                               */
+/* @PutMapping                       -> Informa que o método/end point será um serviço da API - responderá pelo verbo PUT.                                */
+/* @@DeleteMapping                   -> Informa que o método/end point será um serviço da API - responderá pelo verbo DELETE.                             */
+/* @PathVariable                     -> O parâmetro é obrigatório na URI. indica para o spring que o parâmetro será a variável definida na rota "/{id}".  */
+/* @RequestParam                     -> O parâmetro é opcional. Indica para o spring que  o parâmetro pode ou não ser passado pela URI.                   */
+/* @RequestBody                      -> Para que o end-point reconheça o objeto enviado na requisição e "case" o objeto com o parâmetro do método insert. */    
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+	
+	// Injetando uma dependência com a camada de serviço.
+	@Autowired
+	private ProductService service;
+	
+	// Endpoint para listar todas as categorias e encapsular uma resposta HTTP. 
+	@GetMapping
+	public ResponseEntity<Page<ProductDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+			) {
+		
+		// Instanciando o objeto que irá realizar a paginação dos registros recuperado pelo banco de dados.
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		// Pode-se retornar a lista de duas formas:
+		// List<ProductDTO> list -> Retorna todos os registros do banco de dados, sem paginação.
+		//                           List é uma interface, por isso se inicializa a variável com ArrayList, que é uma das implementações da List.
+		// Page<ProductDTO> list -> Retorna todos os registros do banco de dados, com paginação.
+		Page<ProductDTO> list = service.findAllPaged(pageRequest);
+		
+		// Retornando a lista de categorias no corpo da resposta HTTP da requisição.
+		// ResponseEntity.ok informa que o código de retorno é 200 - Sucesso.
+		return ResponseEntity.ok().body(list); 
+	}
+	
+	// Endpoint para recuperar uma categoria pelo ID e encapsular uma resposta HTTP. 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		
+		ProductDTO dto = service.findById(id);
+		
+		// Retornando a categoria no corpo da resposta HTTP da requisição.
+		// ResponseEntity.ok informa que o código de retorno é 200 - Sucesso.
+		return ResponseEntity.ok().body(dto); 
+	}
+	
+	// Endpoint para inserir uma categoria e encapsular uma resposta HTTP. 
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+		dto = service.insert(dto);
+		
+		// Por padrão o ResponseEntity.ok() retorna o código 200. Porém, na inclusão de um novo registro, o código recomendado é o 201 (recurso criado).
+		// Retorna no cabeçalho da resposta o endereço do recurso criado.
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto); 
+	}
+	
+	// Endpoint para alterar uma categoria e encapsular uma resposta HTTP. 
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+		dto = service.update(id, dto);
+		
+		// Retornando a categoria no corpo da resposta HTTP da requisição.
+		// ResponseEntity.ok informa que o código de retorno é 200 - Sucesso.
+		return ResponseEntity.ok().body(dto); 
+	}
+	
+	// Endpoint para excluir uma categoria. 
+	// O retorno do delete também pode ser um ResponseEntity<Void>, já que não será necessário retornar um DTO no corpo da requisição.
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		
+		// No delete a resposta não precisa ter corpo
+		// ResponseEntity.noContent().build() -> Responde um código 204 - No content.
+		return ResponseEntity.noContent().build(); 
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -0,0 +1,87 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.Optional;
+import javax.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+
+	// Autowired instancia automaticamente o objeto anotado, neste caso o repository.
+	@Autowired private ProductRepository repository;
+	
+	// @Transactional -> O próprio framework spring envolve o método em uma transação com o banco de dados.
+	// readOnly = true evita que o banco de dados seja lockado, melhorando assim a performance.
+	//                 Mais utilizado em métodos que fazem apenas leitura de dados.
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Product> list = repository.findAll(pageRequest);
+		
+		// .map() -> Transforma cada cada elemento original em outra coisa. Ela aplica uma função a cada elemento da lista.
+		// x -> new ProductDTO(x) -> Para cada elemento da lista chama a função ProductDTO(x), que transforma o objeto Product em ProductDTO. 
+		return list.map(x -> new ProductDTO(x)); 
+	}
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		// O objeto Optional surgiu a partir do Java 8 para evitar que se trabalhe com valor nulo.
+		// O retorno desta busca nunca será um valor nulo.
+		Optional<Product> obj = repository.findById(id);
+		
+		// Extraindo o objeto Product de dentro do Optional;
+		// orElseThrow() -> Permite levantar uma exceção caso a recuperação do objeto falhe.
+		Product entity = obj.orElseThrow( () -> new ResourceNotFoundException( "Objeto não encontrado!" ) );
+
+		return new ProductDTO(entity, entity.getCategories());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+//		entity.setName(dto.getName());
+		
+		// O "save", por padrão, retorna uma referência para a entidade salva. Por isso é necessário atualizar a variável local.
+		entity = repository.save(entity);
+		
+		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		
+		try {
+			// Neste caso usa-se o getOne() e não o findById().
+			// O getOne() -> Não acessa o banco de dados. Ele apenas instancia um objeto provisório. Apenas quando salvar é que ele fará o acesso ao banco de dados.
+			// Esse método deve ser usado para atualizar registros, evitando assim um acesso a mais ao banco, que seria realizado pelo findById().
+			Product entity = repository.getOne(id);
+//			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+			
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("ID não encontrado: " + id);
+		}
+	}
+
+	// O delete não tem o @Transactional, pois ela impede a captura de algumas exceções, como a EmptyResultDataAccessException e a DataIntegrityViolationException.  
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("ID não encontrado: " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Violação de integridade");
+		}
+	}
+}
