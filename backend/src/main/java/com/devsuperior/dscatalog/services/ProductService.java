f694f2c7d4507cdcd20f0a133025fd989cf3a6c7
David Ferreira da Cunha <david.ferreira@ao3tech.com>
2021-09-26 23:10:12 -0300

    Product insert, update and delete

--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -9,8 +9,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -20,6 +23,7 @@ public class ProductService {
 
 	// Autowired instancia automaticamente o objeto anotado, neste caso o repository.
 	@Autowired private ProductRepository repository;
+	@Autowired private CategoryRepository categoryRepository;
 	
 	// @Transactional -> O próprio framework spring envolve o método em uma transação com o banco de dados.
 	// readOnly = true evita que o banco de dados seja lockado, melhorando assim a performance.
@@ -49,8 +53,7 @@ public class ProductService {
 	@Transactional
 	public ProductDTO insert(ProductDTO dto) {
 		Product entity = new Product();
-//		entity.setName(dto.getName());
-		
+		copyDTOToEntity(dto, entity);
 		// O "save", por padrão, retorna uma referência para a entidade salva. Por isso é necessário atualizar a variável local.
 		entity = repository.save(entity);
 		
@@ -65,7 +68,7 @@ public class ProductService {
 			// O getOne() -> Não acessa o banco de dados. Ele apenas instancia um objeto provisório. Apenas quando salvar é que ele fará o acesso ao banco de dados.
 			// Esse método deve ser usado para atualizar registros, evitando assim um acesso a mais ao banco, que seria realizado pelo findById().
 			Product entity = repository.getOne(id);
-//			entity.setName(dto.getName());
+			copyDTOToEntity(dto, entity);
 			entity = repository.save(entity);
 			return new ProductDTO(entity);
 			
@@ -82,6 +85,20 @@ public class ProductService {
 			throw new ResourceNotFoundException("ID não encontrado: " + id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException("Violação de integridade");
+		}
+	}
+	
+	private void copyDTOToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+		
+		entity.getCategories().clear();
+		for (CategoryDTO catDTO : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDTO.getId());
+			entity.getCategories().add(category);
 		}
 	}
 }
